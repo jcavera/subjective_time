e6b7af9573c7130cbd34bc73990b794da296a687
Jim Cavera <j_cavera@yahoo.com>
2026-01-06 15:04:09 -0800

    Update auto_start.bat

--- auto_start.bat
+++ auto_start.bat
@@ -1,2 +1,9 @@
+:: This is an auto-starter for the windows app. If you want to use it, change the path to
+:: wherever you put the init.py file (the one that's a part of this project, duh) and put
+:: the autostart.bat file (or a link to it, whatever) into the windows startup items.  It
+:: may be necessary to add python to your environment variables or replace "python" with
+:: a link to the actual python.exe on your system.  Whatever, you get the idea.
+
 python C:\Users\pc\Documents\VBShare\python_app_01\__init__.py
 cmd /k
+
